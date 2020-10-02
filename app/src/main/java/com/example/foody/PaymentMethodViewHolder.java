ac1916331505205530f3ca99fcf5819a9824bc72
Rusiru Abhisheak <rusiruavb98@gmail.com>
2020-10-02 19:54:50 +0530

    RAVB- Add payment method feature 2.0

---
+++ app/src/main/java/com/example/foody/PaymentMethodViewHolder.java
@@ -0,0 +1,29 @@
+package com.example.foody;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PaymentMethodViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView cardNumber;
+    public TextView cardHolderName;
+    public TextView paymentUserPhoneNumber;
+    public TextView validationPeriod;
+    public Button deletePaymentMethod;
+    public CardView paymentMethodCard;
+
+    public PaymentMethodViewHolder(@NonNull View itemView) {
+        super(itemView);
+        cardNumber = itemView.findViewById(R.id.seller_profile_item_name);
+        cardHolderName = itemView.findViewById(R.id.display_payment_holder_name);
+        paymentUserPhoneNumber = itemView.findViewById(R.id.display_payment_phone_number);
+        validationPeriod = itemView.findViewById(R.id.display_payment_validation_period);
+        deletePaymentMethod = itemView.findViewById(R.id.payment_item_delete_button);
+        paymentMethodCard = itemView.findViewById(R.id.payment_item_card);
+    }
+}
